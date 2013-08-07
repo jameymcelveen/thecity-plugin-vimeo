76afeeee11fd8bc586d1e18ab0559b014a781d7e
Jamey McElveen <jamey.mcelveen@acstechnologies.com>
2013-08-07 11:58:12 -0400

    rename javascript

---
+++ tcplugin.helper.js
@@ -0,0 +1,31 @@
+// Create a namespace for TheCity if we ain't got one
+if (TheCity == null || typeof(TheCity) != "object") { var TheCity = {}; }
+
+TheCity.PluginHelper = function() {
+	
+	var checkjQuery = function() {
+		if (jQuery != null) return true;
+		alert('jQuery is is required for tc-plugin-helper.js. You can add this to your html to fix this issue. <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>');
+		return false;
+	};
+	
+	// Cross Domain Post Message 
+	var cache_bust = 1, window = this;
+	var crossDomainPostMessage = function(message, target_url, target) {
+		if (!target_url) return;
+		target = target || parent;
+		if (window['postMessage']) target['postMessage'](message, target_url.replace( /([^:]+:\/\/[^\/]+).*/, '$1'));
+		else if (target_url) target.location = target_url.replace(/#.*$/, '') + '#' + (+new Date) + (cache_bust++) + '&' + message;
+	};
+	
+    return {
+		// resize the containing IFrame to be tall enough to display all the content
+		// in the cild documemnt
+		initPlugin: function(subdomain) {
+			if (!checkjQuery()) return ;
+			var src = 'https://' + subdomain + '.onthecity.org/#' + encodeURIComponent(document.location.href);
+			var documentHeight = $(document).height();
+			crossDomainPostMessage(documentHeight, src, frames[0]);
+		}
+    };
+}();
